10b3173c6951eae103380337f0c0c4222d1e6a59
indirafix03 <indiraramayani14@gmail.com>
2025-05-12 21:12:53 +0800

    validator user

--- src/main/java/org/kelompok5/utils/Validator.java
+++ src/main/java/org/kelompok5/utils/Validator.java
@@ -11,7 +11,7 @@ public class Validator {
         System.out.print("> ");
         String string = scanner.nextLine();
 
-        while (string.isEmpty()) {
+        while (string.trim().isEmpty()) {
             System.out.println("ERROR: " + pesanError);
             System.out.print("> ");
             string = scanner.nextLine();
@@ -22,14 +22,69 @@ public class Validator {
     public int inputInt(String placehorder, String pesanError) {
         System.out.println(placehorder);
         System.out.println("> ");
-        int integer = scanner.nextInt();
 
-        while (integer == 0) {
+        while (!scanner.hasNextInt()) {
             System.out.println("Error: " + pesanError);
             System.out.println("> ");
-            integer = scanner.nextInt();
+            scanner.nextInt(); // buang input invalid
 
         }
-        return integer;
+        return scanner.nextInt();
     }
+
+    // Khusus validator kelas user id,nama, nim, nilai, password
+    public String InputId(String placeholder, String pesanError) {
+        String id = inputString(placeholder, pesanError);
+        while (!id.matches("[A-Za-Z].*")) {
+            System.out.println("Error: " + pesanError);
+            id = inputString(placeholder, pesanError);
+        }
+        return id;
+    }
+
+    public String inputNama(String placeholder, String pesanError) {
+        String nama = inputString(placeholder, pesanError);
+        while (nama.length() < 3) {
+            System.out.println("ERROR: " + pesanError);
+            nama = inputString(placeholder, pesanError);
+        }
+        return nama;
+    }
+
+    public String inputNIM(String placeholder, String pesanError) {
+        String nim = inputString(placeholder, pesanError);
+        while (!nim.matches("\\d{10,}")) {
+            System.out.println("ERROR: " + pesanError);
+            nim = inputString(placeholder, pesanError);
+        }
+        return nim;
+    }
+
+    public double inputNilai(String placeholder, String pesanError) {
+        double nilai = -1;
+        while (true) {
+            String input = inputString(placeholder, pesanError);
+            try {
+                nilai = Double.parseDouble(input);
+                if (nilai >= 0 && nilai <= 100) {
+                    break;
+                } else {
+                    System.out.println("ERROR: " + pesanError);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: " + pesanError);
+            }
+        }
+        return nilai;
+    }
+
+    public String inputPassword(String placeholder, String pesanError) {
+        String password = inputString(placeholder, pesanError);
+        while (password.length() < 6) {
+            System.out.println("ERROR: " + pesanError);
+            password = inputString(placeholder, pesanError);
+        }
+        return password;
+    }
+
 }
